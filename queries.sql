913b95ef6f926849c76803fbbd6b48e47f8cc192
adelguit <adeelguitoun@gmail.com>
2022-12-06 11:34:00 +0100

    select AVG of escpaing attemps for animals, grouped by species filtred by date of born between 1990 and 2000

--- queries.sql
+++ queries.sql
@@ -87,3 +87,8 @@ GROUP BY neutered;
 -- [X] What is the minimum and maximum weight of each type of animal?
 SELECT species, MIN(weight_kg) AS "MIN weight" FROM animals
 GROUP BY species;
+
+-- [X] What is the average number of escape attempts per animal type of those born between 1990 and 2000?
+SELECT species, CAST(AVG(escape_attempts) AS DECIMAL(10)) AS "AVG of escaping" FROM animals
+WHERE date_of_birth BETWEEN '1990-01-01' AND '2000-12-31'
+GROUP BY species;
