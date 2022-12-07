78354afad47c02e741893b0ba7f471478af40224
adelguit <adeelguitoun@gmail.com>
2022-12-07 18:03:37 +0100

    Insert data into specializations

--- data.sql
+++ data.sql
@@ -120,3 +120,81 @@ INSERT INTO vets (name, age, date_of_graduation)
 -- [] Vet Jack Harkness is 38 years old and graduated Jun 8th, 2008.
 INSERT INTO vets (name, age, date_of_graduation)
   VALUES('Jack Harkness', 38, 'Jun 08, 2008');
+
+-- Insert the following data for specialties:
+-- [X] Vet William Tatcher is specialized in Pokemon.
+INSERT INTO specializations (vet_id, species_id)
+  VALUES(
+    (SELECT id FROM vets WHERE name = 'William Tatcher'),
+    (SELECT id FROM species WHERE name = 'Pokemon')
+  );
+
+-- [X] Vet Stephanie Mendez is specialized in Digimon and Pokemon.
+INSERT INTO specializations (vet_id, species_id)
+  VALUES(
+    (SELECT id FROM vets WHERE name = 'Stephanie Mendez'),
+    (SELECT id FROM species WHERE name = 'Digimon')
+  );
+
+INSERT INTO specializations (vet_id, species_id)
+  VALUES(
+    (SELECT id FROM vets WHERE name = 'Stephanie Mendez'),
+    (SELECT id FROM species WHERE name = 'Pokemon')
+  );
+
+-- [X] Vet Jack Harkness is specialized in Digimon.
+INSERT INTO specializations (vet_id, species_id)
+  VALUES(
+    (SELECT id FROM vets WHERE name = 'Jack Harkness'),
+    (SELECT id FROM species WHERE name = 'Digimon')
+  );
+
+-- Insert the following data for visits:
+-- [] Agumon visited William Tatcher on May 24th, 2020.
+-- [] Agumon visited Stephanie Mendez on Jul 22th, 2020.
+
+-- [] Gabumon visited Jack Harkness on Feb 2nd, 2021.
+
+-- [] Pikachu visited Maisy Smith on Jan 5th, 2020.
+
+-- [] Pikachu visited Maisy Smith on Mar 8th, 2020.
+
+-- [] Pikachu visited Maisy Smith on May 14th, 2020.
+
+-- [] Devimon visited Stephanie Mendez on May 4th, 2021.
+
+-- [] Charmander visited Jack Harkness on Feb 24th, 2021.
+
+-- [] Plantmon visited Maisy Smith on Dec 21st, 2019.
+
+-- [] Plantmon visited William Tatcher on Aug 10th, 2020.
+
+-- [] Plantmon visited Maisy Smith on Apr 7th, 2021.
+
+-- [] Squirtle visited Stephanie Mendez on Sep 29th, 2019.
+
+-- [] Angemon visited Jack Harkness on Oct 3rd, 2020.
+
+-- [] Angemon visited Jack Harkness on Nov 4th, 2020.
+
+-- [] Boarmon visited Maisy Smith on Jan 24th, 2019.
+
+-- [] Boarmon visited Maisy Smith on May 15th, 2019.
+
+-- [] Boarmon visited Maisy Smith on Feb 27th, 2020.
+
+-- [] Boarmon visited Maisy Smith on Aug 3rd, 2020.
+
+-- [] Blossom visited Stephanie Mendez on May 24th, 2020.
+
+-- [] Blossom visited William Tatcher on Jan 11th, 2021.
+
+
+
+INSERT INTO specializations (vet_id, species_id)
+  VALUES(
+    ((SELECT id FROM vets WHERE name = 'Stephanie Mendez'),
+    (SELECT id FROM species WHERE name = 'Pokemon')),
+    ((SELECT id FROM vets WHERE name = 'Stephanie Mendez'),
+    (SELECT id FROM species WHERE name = 'Digimon'))
+  );
